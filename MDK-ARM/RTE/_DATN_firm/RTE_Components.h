eea6f87960477c9682052e53d4a889ef5bffe8d2
nguyenvietanh <vietanh270501@gmail.com>
2023-12-13 08:52:57 +0700

    firm_13-12

---
+++ MDK-ARM/RTE/_DATN_firm/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DATN_firm' 
+ * Target:  'datn_firm' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
